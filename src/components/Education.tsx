e0fc1caf5860780b44112e099300bdb566517b7e
VIDAL-Antoine <avidal78390@gmail.com>
2025-11-22 22:53:44 +0100

    feat: use chip for education contents

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,10 +1,39 @@
-import React from "react";
 import '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import '../assets/styles/Education.scss'
+import { Chip, ChipProps } from '@mui/material';
+
+const labelsISTY = [
+    "Programmation Orientée Objet",
+    "C",
+    "Linux",
+    "Bases de données",
+    "Systèmes d'exploitation",
+    "Compilateurs",
+    "Architecture des ordinateurs",
+    "Algorithmique",
+    "Architecture logicielle",
+];
+
+const labelsHPC = [
+    "Big Data",
+    "Parallélisme",
+    "Multithreading",
+];
+
+const EducationChip = (props: ChipProps) => (
+  <Chip
+    {...props}
+    sx={{
+      margin: '4px',
+      '& .MuiChip-label': { fontFamily: '"Lato", sans-serif' },
+      ...props.sx
+    }}
+  />
+);
 
 interface EducationProps { lang: 'fr' | 'en' }
 
@@ -24,7 +53,11 @@ const Education = ({ lang }: EducationProps) => {
           >
             <h3 className="vertical-timeline-element-title">Ingénieur informatique</h3>
             <h4 className="vertical-timeline-element-subtitle">ISTY, UVSQ Paris-Saclay - Vélizy</h4>
-            <p>Programmation Orientée Objet, C, Linux, Bases de données, Systèmes d'exploitation, Compilateurs, Architec- ture des ordinateurs, Algorithmique, Architecture logicielle</p>
+            <div style={{ marginTop: '8px' }}>
+              {labelsISTY.map((label) => (
+                <EducationChip key={label} label={label} />
+              ))}
+            </div>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
@@ -34,7 +67,11 @@ const Education = ({ lang }: EducationProps) => {
           >
             <h3 className="vertical-timeline-element-title">Master Calcul Haute Performance</h3>
             <h4 className="vertical-timeline-element-subtitle">LI-PaRAD, UVSQ Paris-Saclay - Guyancourt</h4>
-            <p>Big Data, Parallélisme, Multithreading</p>
+            <div style={{ marginTop: '8px' }}>
+              {labelsHPC.map((label) => (
+                <EducationChip key={label} label={label} />
+              ))}
+            </div>
           </VerticalTimelineElement>
         </VerticalTimeline>
       </div>
