4eac36eb78fbd094133777a55f58eec19de66ffb
VIDAL-Antoine <avidal78390@gmail.com>
2025-11-23 18:28:59 +0100

    fix: make site compatible with github static pages

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,42 +1,47 @@
 import '../assets/styles/Project.scss';
 import { useTranslation } from 'react-i18next';
 
+const images = import.meta.glob('../assets/images/projects/*', { eager: true, import: 'default' });
+
 const Project = () => {
     const { t } = useTranslation();
 
-    const projects = t("projects.items", { returnObjects: true }) as {
+    const projects = (t("projects.items", { returnObjects: true }) as {
         title: string;
         description: string;
         image: string;
         link?: string;
-    }[];
+    }[]).map(project => ({
+        ...project,
+        image: images[`../assets/images/projects/${project.image}`] as string
+    }));
 
-    return(
-    <div className="projects-container" id="projects">
-        <h1>{t("projects.title")}</h1>
-        <div className="projects-grid">
-            {projects.map((project, index) => (
-                <div className="project" key={index}>
-                    {project.link ? (
-                        <a href={project.link} target="_blank" rel="noreferrer">
-                            <img src={project.image} className="zoom" alt="thumbnail" width="100%" />
-                        </a>
-                    ) : (
-                        <img src={project.image} className="zoom" alt="thumbnail" width="100%" />
-                    )}
-                    {project.link ? (
-                        <a href={project.link} target="_blank" rel="noreferrer">
+    return (
+        <div className="projects-container" id="projects">
+            <h1>{t("projects.title")}</h1>
+            <div className="projects-grid">
+                {projects.map((project, index) => (
+                    <div className="project" key={index}>
+                        {project.link ? (
+                            <a href={project.link} target="_blank" rel="noreferrer">
+                                <img src={project.image} className="zoom" alt={project.title} width="100%" />
+                            </a>
+                        ) : (
+                            <img src={project.image} className="zoom" alt={project.title} width="100%" />
+                        )}
+                        {project.link ? (
+                            <a href={project.link} target="_blank" rel="noreferrer">
+                                <h2>{project.title}</h2>
+                            </a>
+                        ) : (
                             <h2>{project.title}</h2>
-                        </a>
-                    ) : (
-                        <h2>{project.title}</h2>
-                    )}
-                    <p dangerouslySetInnerHTML={{ __html: project.description }} />
-                </div>
-            ))}
+                        )}
+                        <p dangerouslySetInnerHTML={{ __html: project.description }} />
+                    </div>
+                ))}
+            </div>
         </div>
-    </div>
     );
-}
+};
 
 export default Project;
